ef3f9c95e5f7f08b9e8cba2bed77badf82043158
HiraAkshayy <henzoageorges@gmail.com>
2023-03-01 12:00:51 +0100

    ouah tqt la version de fou

--- main.c
+++ main.c
@@ -3,13 +3,12 @@
 #include <stdbool.h>
 
 #include "constantes.h"
-#include "jeu.h"
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 
-void deplacerPers(int carte[][26], SDL_Rect *pos, int direction, SDL_Renderer *render){
+void deplacerPers(int carte[][480], SDL_Rect *pos, int direction, SDL_Renderer *render){
     switch(direction){
         case HAUT: pos->y--;break;
         case BAS: pos->y++;break;
@@ -19,35 +18,45 @@ void deplacerPers(int carte[][26], SDL_Rect *pos, int direction, SDL_Renderer *r
     }
 }
 
-/*int jouer(SDL_Surface *ecran){
-    SDL_Rect position, positionJoueur;
+int jouer(SDL_Renderer *render){
+    SDL_Rect position, positionJoueur, backRect;
     SDL_Surface *sPerso[5] = {NULL};
+    SDL_Surface *sBackground = NULL;
     SDL_Texture *tPerso[5] = {NULL};
+    SDL_Texture *tBackground = NULL;
     SDL_Texture *persoActuel = NULL;
 
     int i;
     int j;
 
-    sPerso[1] = IMG_LoadPNG_RW("cat.png", "rb");
+    SDL_RWops *rwop = SDL_RWFromFile("background.png", "rb");
+    sBackground = IMG_LoadPNG_RW(rwop);
 
-    int carte[11][26];
+    rwop = SDL_RWFromFile("player.png", "rb");
+    sPerso[0] = IMG_LoadPNG_RW(rwop);
 
-    for(j = 1; j < 5; j++){
+    int carte[640][480];
+    int shop[640][480];
+    int mine[640][480];
+
+    /*for(j = 0; j < 5; j++){
         if(sPerso[j] == NULL){
-            printf("%d : Erreur SDL chargement de l'imiage Personnage \n", j);
+            printf("%d : Erreur SDL chargement de l'image Personnage \n", j);
             return -1;
         }
-    }
+    }*/
 
-    for(i = 1; i < 5; i++){
+    for(i = 0; i < 5; i++){
         tPerso[i] = SDL_CreateTextureFromSurface(render, sPerso[i]);
         SDL_FreeSurface(sPerso[i]);
         SDL_Log("Free de la surface du perso %d", i);
     }
+    tBackground = SDL_CreateTextureFromSurface(render, sBackground);
+    SDL_FreeSurface(sBackground);
 
     SDL_RenderClear(render);
 
-    persoActuel = tPerso[BAS];
+    persoActuel = tPerso[0];
     if(persoActuel == NULL){
         SDL_Log("Erreur lors du chargement du personnage actuelle");
     }
@@ -56,15 +65,20 @@ void deplacerPers(int carte[][26], SDL_Rect *pos, int direction, SDL_Renderer *r
     position.w = 100;
     position.h = 100;
 
+    backRect.w = 1280;
+    backRect.h = 980;
+
     bool is_running = true;
     SDL_Event event;
     while(is_running){
+        SDL_RenderClear(render);
         while(SDL_PollEvent(&event)){
             switch(event.type){
                 case SDL_QUIT: is_running = false;break;
 
                 case SDL_KEYDOWN:
                     switch(event.key.keysym.sym){
+                        case SDLK_ESCAPE: is_running = false; break;
                         case SDLK_UP: deplacerPers(carte, &positionJoueur, HAUT, render); break;
                         case SDLK_DOWN: deplacerPers(carte, &positionJoueur, BAS, render); break;
                         case SDLK_LEFT: deplacerPers(carte, &positionJoueur, GAUCHE, render); break;
@@ -74,10 +88,16 @@ void deplacerPers(int carte[][26], SDL_Rect *pos, int direction, SDL_Renderer *r
                 default: break;
             }
         }
+        if(positionJoueur.x > 640){
+            positionJoueur.x = 3;
+            position.x = positionJoueur.x * TAILLE_BLOCK;
+        }
         position.x = positionJoueur.x * TAILLE_BLOCK;
         position.y = positionJoueur.y * TAILLE_BLOCK;
 
-        SDL_BlitSurface(persoActuel, NULL, ecran)
+        if(SDL_RenderCopy(render, tBackground, NULL, &backRect) != 0){
+            SDL_Log("Erreur lors de l'affichage à l'écran");
+        }
 
         if(SDL_RenderCopy(render, persoActuel, NULL, &position) != 0){
             SDL_Log("Erreur lors de l'affichage à l'écran");
@@ -88,21 +108,19 @@ void deplacerPers(int carte[][26], SDL_Rect *pos, int direction, SDL_Renderer *r
     SDL_DestroyRenderer(render);
     SDL_Quit();
     return -1;
-}*/
+}
 
 int main(){
     if(SDL_Init(SDL_INIT_VIDEO) != 0){
         printf("error initializing SDL: %s\n", SDL_GetError());
     }
     SDL_Window *win = NULL;
-    win = SDL_CreateWindow("Le Mans Valley", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
+    win = SDL_CreateWindow("Daniels' Valley", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
 
     if(win){
-        jouer(win);
-        SDL_Delay(3000);
-        SDL_DestroyWindow(win);
+        //Création d'un render un des positions du personnage
+        SDL_Renderer *render = SDL_CreateRenderer(win, -1, 0);
+        jouer(render);
     }
-
-    SDL_Quit();
     return 0;
 }
